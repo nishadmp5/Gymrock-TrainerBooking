c5cceab5eab4076c08b5b6861f7ebeed1a603105
Nishad <mohdnishadmp@gmail.com>
2025-02-12 13:09:26 +0530

    bcrypt issue solved

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import validator from 'validator'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { v2 as cloudinary} from 'cloudinary'
 import trainerModel from '../models/trainerModel.js'
 import jwt from 'jsonwebtoken'
--- backend/controllers/trainerController.js
+++ backend/controllers/trainerController.js
@@ -1,5 +1,5 @@
 import trainerModel from '../models/trainerModel.js'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import appointmentModel from '../models/appointmentModel.js'
 
