6e20c442c0193035b9636c1b68e161f6812e795e
Nishad <mohdnishadmp@gmail.com>
2025-03-15 18:09:58 +0530

    reload issue

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,5 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174}
-})
+  server:{port:5174},
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: '/',  // Ensures correct asset paths
+});
